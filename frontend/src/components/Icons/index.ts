49deb6fca8878ed66ef089b38aafa5c02cf2191d
Braulio Henrique Marques Souto <braulio@disroot.org>
2024-07-13 03:19:40 -0300

    Adicionando novos Icones

--- frontend/src/components/Icons/index.ts
+++ frontend/src/components/Icons/index.ts
@@ -3,5 +3,7 @@ export * from "./ArrowTrendingUpIcon";
 export * from "./ChevronDownIcon";
 export * from "./ChevronLeftIcon";
 export * from "./ChevronRightIcon";
+export * from "./EyeIcon";
+export * from "./PencilIcon";
 export * from "./PlusCircleIcon";
 export * from "./PointIcon";
